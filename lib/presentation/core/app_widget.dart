2d0ffbb28ea841c43f83a96c30da041f735480cf
Calvin Ellis <calvinellis@Gumdum.local>
2021-03-31 00:37:26 +0200

    updated multibloc from episode 10

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes2/application/auth/auth_bloc.dart';
+import 'package:notes2/injection.dart';
 import 'package:notes2/presentation/routes/router.gr.dart';
 
 class AppWidget extends StatelessWidget {
@@ -6,28 +9,36 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final _appRouter = AppRouter();
 
-    return MaterialApp.router(
-      title: 'Notes 2',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        primaryColor: Colors.green[800],
-        accentColor: Colors.blueAccent,
-        inputDecorationTheme: InputDecorationTheme(
-            border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-        )),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            primary: Colors.black,
-            backgroundColor: Colors.amberAccent,
-            textStyle: const TextStyle(
-              fontWeight: FontWeight.bold,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+        )
+      ],
+      child: MaterialApp.router(
+        title: 'Notes 2',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light().copyWith(
+          primaryColor: Colors.green[800],
+          accentColor: Colors.blueAccent,
+          inputDecorationTheme: InputDecorationTheme(
+              border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+          )),
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              primary: Colors.black,
+              backgroundColor: Colors.amberAccent,
+              textStyle: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
+        routerDelegate: _appRouter.delegate(),
+        routeInformationParser: _appRouter.defaultRouteParser(),
       ),
-      routerDelegate: _appRouter.delegate(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
 }
